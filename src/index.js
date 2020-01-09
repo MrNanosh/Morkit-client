3700fbc2a56b426ce22f507d93641cc611625740
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-01-09 10:27:33 -0800

    in the middle of doing a dry production run

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App/App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
-import { library } from '@fortawesome/fontawesome-svg-core';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App/App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faPlus,
   faChevronLeft,
   faTrashAlt,
   faCheckDouble,
   faBreadSlice
-} from '@fortawesome/free-solid-svg-icons';
-library.add(
-  faPlus,
-  faChevronLeft,
-  faTrashAlt,
-  faCheckDouble,
-  faBreadSlice
-);
+} from "@fortawesome/free-solid-svg-icons";
+library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble, faBreadSlice);
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
